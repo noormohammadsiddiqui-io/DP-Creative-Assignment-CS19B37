417bfbf4fbb95d01b85ec75a8b4abbe73eeca811
Noor Mohammad Siddiqui <noors.cseb19@sbjit.edu.in>
2022-04-27 14:50:32 +0530

    first commit

---
+++ PushNotification.java
@@ -0,0 +1,9 @@
+public class PushNotification implements Notification {
+
+	@Override
+	public void notifyUser()
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Sending a push notification");
+	}
+}
